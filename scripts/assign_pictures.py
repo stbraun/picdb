8e8fbe7a4f4b09f087829ad1baace3a4a8d1d20d
sb <sb@action.ms>
2016-09-17 11:36:32 +0200

    Setup added for deployment. First tries with py2app - failed.

--- scripts/assign_pictures.py
+++ scripts/assign_pictures.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # coding=utf-8
 """
 Add pictures selected by path to given series.
--- setup.py
+++ setup.py
@@ -1,17 +1,44 @@
-
 import sys
-from distutils.core import setup
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    """This is a plug-in for setuptools.
+
+     It will invoke py.test when you run python setup.py test
+    """
+
+    def finalize_options(self):
+        """Configure."""
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        """Execute tests."""
+        # import here, because outside the required eggs aren't loaded yet
+        import pytest
+        sys.exit(pytest.main(self.test_args))
+
+
+version = '0.1'
 
 setup(
     name='picdb',
-    version='0.1.0dev',
-    packages=[''],
-    package_dir={'': 'picdb'},
+    version=version,
+    packages=['picdb'],
     url='',
-    license='BSD',
+    license='MIT',
     author='Stefan Braun',
-    author_email='',
-    description='Picture Database',
+    author_email='sb@action.ms',
+    description='Simple image database.',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Pillow', 'PyYAML'],
+    requires=['Pillow', 'PyYAML'],
     provides=['picdb'],
-    app=["picdb/main.py"]
+    scripts=['scripts/assign_pictures.py', 'start_picdb.py'],
+    tests_require=['pytest', 'nose'],
+    cmdclass={'test': PyTest},
 )
