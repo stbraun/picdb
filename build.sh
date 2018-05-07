180d81f8dd0389e3917b189bcfbe72befa57e38b
sb <sb@action.ms>
2018-05-07 21:56:30 +0200

    move all into build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,22 @@
 
 source venv/bin/activate
 
+rm -rf .coveraage
+rm -rf reports
+mkdir reports
+rm -rf venv
+
+python3 -m venv venv
+
 pip install -r requirements.txt
 
 flake8 --output-file reports/flake8.txt --benchmark --count --statistics picdb start_picdb.py
 
-nosetests --with-coverage --cover-branches --cover-inclusive --cover-html --cover-html-dir=reports/coverage --cover-xml --cover-xml-file=reports/coverage.xml test/
+pylint --rcfile=resrc/pylintrc picdb > reports/pylint.txt
 
-paver docs
+nosetests --with-coverage --cover-branches --cover-inclusive --with-xunit --xunit-file=reports/nosetests.xml --cover-html --cover-html-dir=reports/coverage --cover-xml --cover-xml-file=reports/coverage.xml test/
 
 paver sdist
+
+cd docs
+make html
