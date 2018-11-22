cff3ba71e48e615a07ade1a0e1b521a3e9f897e8
sb <sb@action.ms>
2018-11-22 19:49:53 +0100

    cleanup.

--- create_app.sh
+++ create_app.sh
@@ -2,11 +2,7 @@
 
 if [ $1 = -n ]; then
     echo "Creating new spec file. Do not forget to add required extensions!"
-    pyinstaller --onefile --windowed \
-                -n PicDB \
-                --additional-hooks-dir pyinstaller-hooks \
-                --runtime-hook pyinstaller-hooks/pyi_rth__tkinter.py \
-                start_picdb.py
+    pyinstaller --onefile --windowed -n PicDB start_picdb.py
     shift
 else
     echo "Using existing spec file. Run with -n to create a new one."
